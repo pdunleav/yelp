e8cd08356eabafd1911e25897ef89df2d78d040a
Phelim Dunleavy <dunleavy.phelim@gmail.com>
2018-05-16 10:44:51 +0100

    restaurants and reviews with nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :restaurants do
+    collection do
+      get 'top', to: 'restaurants#top'
+    end
+
+    member do
+      get 'chef', to: 'restaurants#chef'
+    end
+
+    resources :reviews, only: [ :new, :create ]
+  end
 end
